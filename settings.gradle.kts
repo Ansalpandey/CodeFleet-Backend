de800666ad2bfb5d7e9ebeb8e01b15baf563185c
Ansalpandey <pandeyansal77@gmail.com>
2025-02-25 17:38:44 +0530

    Rename project to CodeFleet and update application properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "Cloud-IDE"
+rootProject.name = "CodeFleet-Backend"
--- src/main/kotlin/com/app/cloudide/CodeFleet.kt
+++ src/main/kotlin/com/app/cloudide/CodeFleet.kt
@@ -2,7 +2,6 @@ package com.app.cloudide
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.CrossOrigin
 
 @SpringBootApplication
 class CloudIdeApplication
